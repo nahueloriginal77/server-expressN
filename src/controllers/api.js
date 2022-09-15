34983eb750c9f56f5c05da68adab8e1c7074d9aa
nahueloriginal77 <calveiranahuel7.7@gmail.com>
2022-09-15 13:28:17 -0300

    first commit

---
+++ src/controllers/api.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loadApiEndpoints = void 0;
+const courses_json_1 = __importDefault(require("../../data/courses.json"));
+const loadApiEndpoints = (app) => {
+    app.get("/cursos", (req, res) => {
+        return res.status(200).send(courses_json_1.default);
+    });
+};
+exports.loadApiEndpoints = loadApiEndpoints;
